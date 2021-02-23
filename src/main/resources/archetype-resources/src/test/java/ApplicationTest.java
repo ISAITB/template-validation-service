f0eb824a30894e6a80169e99fb2bf91cfd0290b3
Costas Simatos <costas.simatos@sigmacubed.eu>
2021-02-23 10:54:06 +0100

    Update for release 1.12.0.

--- src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
+++ src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
@@ -1,14 +1,11 @@
 package ${package};
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Unit test to ensure that the Spring context loads.
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTest {
 
